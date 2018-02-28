e8ddf90cc585d1b4d2f62902ea3ceaeb819a1f6c
Varun Gupta <varung3999@gmail.com>
2018-02-28 17:01:26 +0530

    second

--- login/admin.py
+++ login/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import user_type, student, teacher, sc
+from .models import *
 admin.site.register(user_type)
 admin.site.register(student)
 admin.site.register(teacher)
 admin.site.register(sc)
+admin.site.register(courses)
 
 # Register your models here.
